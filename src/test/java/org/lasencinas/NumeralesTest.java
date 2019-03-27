2b0297f0c01e206610daafb43ca16791ef926a75
alexnro <alexjandron99@gmail.com>
2019-03-27 15:59:22 +0100

    Creado fichero para casos test

---
+++ src/test/java/org/lasencinas/NumeralesTest.java
@@ -0,0 +1,16 @@
+package org.lasencinas;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+
+public class NumeralesTest {
+
+    @Test
+    public void numeralesSolosTest() {
+        
+    }
+}
